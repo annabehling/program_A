1f300f4d5ab8c501cd8c39aba1e39dda12719b95
Anna Martinson-Behling <anna.behling93@gmail.com>
2018-12-04 12:54:16 +1300

    part way through editing commenting, but have to restart computer now

--- SNP_finder.py
+++ SNP_finder.py
@@ -1,13 +1,15 @@
 import sys #otherwise sys.exit gives sys is undefined
-from Bio import SeqIO
+from Bio import SeqIO #SeqIO already has methods like convert and others
 def fasta_file(fastq_file, outfile):
-	"""convert fastq file to fasta file"""
-	SeqIO.convert('infile','fastq','output','outfile')
+	"""convert fastq file to fasta file
+	fastq_file : path to a .fq file (str)
+	outfile : path to a .fasta file that will be created (str)"""
+	SeqIO.convert(fastq_file,'fastq',outfile,'fasta')
 
 def fastq_sequence(fastq_file, outfile):
     """takes a fastq file and writes all sequences to a new file
-    infile : .fq file (str)
-    outfile : new file containing output sequences"""
+    fastq_file : path to a .fq file (str)
+    outfile : path to a new file containing output sequences (str)"""
     with open (infile) as fastq_file:
         outhandle = open(outfile, "w")
         for i,line in enumerate(fastq_file):
@@ -17,7 +19,7 @@ def fastq_sequence(fastq_file, outfile):
 def variant_sites(fq_seqs, refseq): #to get sites that are variant
     """takes 2 sequences and returns the position of the variant site and the nucleotide variants
     fq_seqs : fastq sequences (iterable)
-    refseq : reference sequence (str)
+    refseq : path to the reference sequence file (str)
     returns index(1-based), variant, reference base
     """
     var_sites = []
@@ -30,11 +32,11 @@ def variant_sites(fq_seqs, refseq): #to get sites that are variant
                     var_indices.append(index)
     return(var_sites)
 
-def vars_to_vcf(var_sites, chromosome, outfile): #to write the information to an outfile and
- get chromosome
+def vars_to_vcf(var_sites, chromosome, outfile): #to write the information to an outfile and get chromosome
     """takes a list of tuples and returns .vcf file with tab separated values of CHR, POS, REF, ALT, ID, FILTER, INFO
     var_sites : list of tuples
-    chromosome : str
+    chromosome : path to the reference sequence file (str)
+    outfile : path to newly created .vcf file (str)
     the columns ID, FILTER, INFO have been hardcoded"""
     outhandle = open(outfile, 'w')
     outhandle.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format("#CHROM", "POS", "REF", "ALT", "ID", "FILTER", "INFO"))
@@ -42,20 +44,18 @@ def vars_to_vcf(var_sites, chromosome, outfile): #to write the information to an
         outhandle.write('{}\t{}\t{}\t{}\t.\t.\t.\n'.format(chromosome, POS, REF, ALT))
 
 def vcf_file(fastq_file, reffile):
-    """does overall stuff""" #call in def fasta_file , def fastq_sequence , def variant_sites , def_vars_to_vcf under this one function
+    """overarching function that calls in def fasta_file , def fastq_sequence , def variant_sites
+    fastq_file : path to a .fq file (str)
+    reffile : path to the reference sequence file (str)"""
     chromosome = SeqIO.read(reffile,format='fasta') #seqIO.parse to get any reference seq
     fq = SeqIO.parse(fastq_file,format='fastq') #seqIO.parse to get any fastq file
     result = variant_sites(fq, chromosome)
     return(result)
-    #thing for outfile from end of january program
-    #make executable using chmod u+x. very last thing to do at the end of the script
-    #./ in readme
 
-variable_sites = vcf_file("input.fq", "reference.fna") #include this bit
-vars_to_vcf(variable_sites, "reference", "result.vcf") #and this bit
+vars_to_vcf(variable_sites, "reference", "result.vcf")#reffile bottom bit, "result.vcf"#becomes outfile in bottom bit
+    
 
-#only need this for command line executable. Not relevant for ipython notebook usage
-if __name__ == '__main__':
+if __name__ == '__main__': #only need this for command line executable. Not relevant for ipython notebook usage
     try:
         fastq_file = sys.argv[1]
         outfile = sys.argv[2]
@@ -64,5 +64,9 @@ if __name__ == '__main__':
     except IndexError:
         print("Usage: ./SNP_finder.py [fastq_file] [outfile] [reffile]")
         sys.exit(1)
-    vcf_file(fastq_file, outfile, reffile)
+    variable_sites = vcf_file(fastq_file, reffile)
 sys.exit(0)
+
+#chmod u+x SNP_finder.py #make executable using chmod u+x. very last thing to do at the end of the script
+
+#./ in readme
