16f98d7be8654dd39e283e572fc3c6ef55cdeb17
Anna Martinson-Behling <anna.behling93@gmail.com>
2018-12-04 15:27:22 +1300

    finished editing commenting and fixed last code paragraph. Made the script python executable.

--- SNP_finder.py
+++ SNP_finder.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys #otherwise sys.exit gives sys is undefined
 from Bio import SeqIO #SeqIO already has methods like convert and others
 def fasta_file(fastq_file, outfile):
@@ -16,11 +18,11 @@ def fastq_sequence(fastq_file, outfile):
             if i % 4 == 1:
                 outhandle.write(line)
 
-def variant_sites(fq_seqs, refseq): #to get sites that are variant
+def variant_sites(fq_seqs, refseq): #to get sites that are variant, in case you want to do things other than make a vcf file
     """takes 2 sequences and returns the position of the variant site and the nucleotide variants
     fq_seqs : fastq sequences (iterable)
     refseq : path to the reference sequence file (str)
-    returns index(1-based), variant, reference base
+    returns index(1-based), variant, reference base (list of tuples)
     """
     var_sites = []
     var_indices = [] #keep track of sites that we've already found
@@ -32,19 +34,19 @@ def variant_sites(fq_seqs, refseq): #to get sites that are variant
                     var_indices.append(index)
     return(var_sites)
 
-def vars_to_vcf(var_sites, chromosome, outfile): #to write the information to an outfile and get chromosome
+def vars_to_vcf(var_sites, contig_name, outfile): #to write the set of variants to an outfile
     """takes a list of tuples and returns .vcf file with tab separated values of CHR, POS, REF, ALT, ID, FILTER, INFO
     var_sites : list of tuples
-    chromosome : path to the reference sequence file (str)
+    contig_name : name of conttig on reference genome for vcf output
     outfile : path to newly created .vcf file (str)
     the columns ID, FILTER, INFO have been hardcoded"""
     outhandle = open(outfile, 'w')
     outhandle.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format("#CHROM", "POS", "REF", "ALT", "ID", "FILTER", "INFO"))
     for POS, REF, ALT in var_sites:
-        outhandle.write('{}\t{}\t{}\t{}\t.\t.\t.\n'.format(chromosome, POS, REF, ALT))
+        outhandle.write('{}\t{}\t{}\t{}\t.\t.\t.\n'.format(contig_name, POS, REF, ALT))
 
-def vcf_file(fastq_file, reffile):
-    """overarching function that calls in def fasta_file , def fastq_sequence , def variant_sites
+#def vcf_file(fastq_file, reffile):
+    """overarching function that calls in variant_sites, vars_to_vcf
     fastq_file : path to a .fq file (str)
     reffile : path to the reference sequence file (str)"""
     chromosome = SeqIO.read(reffile,format='fasta') #seqIO.parse to get any reference seq
@@ -52,21 +54,18 @@ def vcf_file(fastq_file, reffile):
     result = variant_sites(fq, chromosome)
     return(result)
 
-vars_to_vcf(variable_sites, "reference", "result.vcf")#reffile bottom bit, "result.vcf"#becomes outfile in bottom bit
-    
-
 if __name__ == '__main__': #only need this for command line executable. Not relevant for ipython notebook usage
     try:
         fastq_file = sys.argv[1]
         outfile = sys.argv[2]
         reffile = sys.argv[3]
-        print('i got executed')
     except IndexError:
-        print("Usage: ./SNP_finder.py [fastq_file] [outfile] [reffile]")
+        print("Usage: ./SNP_finder.py [fastq_file] [outfile.vcf] [reffile]") #./ is there so if there's a program else where called SNP_finder.py, it will execute this one
         sys.exit(1)
-    variable_sites = vcf_file(fastq_file, reffile)
+    chromosome = SeqIO.read(reffile,format='fasta') #seqIO.parse to get any reference seq
+    fq = SeqIO.parse(fastq_file,format='fastq') #seqIO.parse to get any fastq file
+    result = variant_sites(fq, chromosome)
+    vars_to_vcf(result, chromosome.id, outfile) #outputs .vcf file
 sys.exit(0)
 
-#chmod u+x SNP_finder.py #make executable using chmod u+x. very last thing to do at the end of the script
-
-#./ in readme
+#chmod u+x SNP_finder.py to make it executable. very last thing to do at the end of the script
